f3a431b5fc4f5bcfa496f16458dd14a25b92fe97
Abigail  Nyameyie Arko <abigail.arko@amalitech.com>
2024-05-06 08:40:43 +0000

    good

--- src/main/java/org/example/quiz1.java
+++ src/main/java/org/example/quiz1.java
@@ -4,8 +4,8 @@ public class quiz1 {
 
     public static void main(String[] args){
 int bankBalance = 500;
-bankBalance += 250;
-        bankBalance =- 100;
+bankBalance +=250;
+        bankBalance -= 100;
         System.out.println("Final bank balance is " +  bankBalance);
 
         double fahrenheit  =75.0;
