0866e765ce153be224f639491fcb2c1365534d81
Abigail  Nyameyie Arko <abigail.arko@amalitech.com>
2024-05-21 16:50:09 +0000

    started with tables

---
+++ src/test/java/Week3/PageClasses/Tablesclass.java
@@ -0,0 +1,30 @@
+package Week3.PageClasses;
+
+import Week3.TestClasses.Tables;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Tablesclass {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practice-automation.com/");
+    }
+
+    @Test
+    public void myTestTable() {
+        Tables tables = new Tables(driver);  // Pass the WebDriver instance
+        tables.testTables();  // Call the method to test tables
+        tables.TableItem();
+    }
+
+
+}
--- src/test/java/Week3/TestClasses/Tables.java
+++ src/test/java/Week3/TestClasses/Tables.java
@@ -1,5 +1,49 @@
 package Week3.TestClasses;
 
-public class Tables
-{
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Tables {
+
+    private final WebDriver driver;
+    private final By Button = By.xpath("//a[normalize-space()='Tables']");
+    private final By price = By.xpath("//td[text()='Oranges']/following-sibling::td");
+    public Tables(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void testTables() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(Button));
+        scrollToElement(button);
+
+        try {
+            button.click();
+        }catch (Exception e){
+            System.out.println("Nomal click failed");
+            javaScriptClick(button);
+        }
+    }
+
+    private void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    private void javaScriptClick(WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+    }
+    public void TableItem(){
+
+        String getText =driver.findElement(price).getText();
+        System.out.println("The price of oranges is"+getText);
+    }
+
+
 }
+
